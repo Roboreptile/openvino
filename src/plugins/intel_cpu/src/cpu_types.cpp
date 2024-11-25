9bef60fb315c244b223e4265b0271d8fb4b6aed6
PiotrKrzem <piotr.krzeminski@intel.com>
2024-11-25 01:11:18 +0000

    [FIX] Add missing enumeration val to switch

--- src/plugins/intel_cpu/src/cpu_types.cpp
+++ src/plugins/intel_cpu/src/cpu_types.cpp
@@ -354,6 +354,7 @@ std::string NameFromType(const Type type) {
         CASE(CumSum);
         CASE(DetectionOutput);
         CASE(ExperimentalDetectronDetectionOutput);
+        CASE(Identity);
         CASE(If);
         CASE(LogSoftmax);
         CASE(TopK);
